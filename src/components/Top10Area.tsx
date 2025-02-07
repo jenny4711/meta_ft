146c7c8b639202abd971a91cc04600c99ecd1c25
jiylee <jjenny4711@gmail.com>
2025-02-07 01:10:40 -0500

    type

--- src/components/Top10Area.tsx
+++ src/components/Top10Area.tsx
@@ -1,9 +1,13 @@
 import SectionBox from './SectionBox'
 import { getUsageByArea} from '@/utlis/apis';
 import styles from './Top10Area.module.css'
-
+interface Top10{
+  country:string;
+  city:string;
+  count:number;
+}
 export default async function Top10Area(){
-const area = await getUsageByArea()
+const area:any = await getUsageByArea()
 
   return(
      
@@ -15,15 +19,15 @@ const area = await getUsageByArea()
   <div className={styles.emBtns}>
 
     {
-     area.map(async(item:any)=>{
+     area.map(async(item:Top10)=>{
      
        const country =item.country? item.country:null
        const city = item.city?item.city:null
-       const count = item.city?item.count:null
+       const count = item.city ? item.count : 0
        const title = `${city}, ${country}`
         return(
           <>
-         {item.city? <SectionBox title={title.toUpperCase()} data={count} width={250} height={100}/>:null}
+         {item.city ? <SectionBox title={title.toUpperCase()} data={count as number} width={250} height={100}/> : null}
           </>
         )
       

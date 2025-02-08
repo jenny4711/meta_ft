2d4ba84e01ad2f8f29a0a6abb45c51459aa68bf5
jiylee <jjenny4711@gmail.com>
2025-02-08 01:14:20 -0500

    fixed eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { dirname } from "path";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,15 +10,65 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  
-    ...compat.rules( {
-      "@typescript-eslint/no-unsed-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",  // `any`를 사용할 수 있도록 비활성화
-      "@typescript-eslint/no-empty-object-type": "off",
-    }),
-  
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends("plugin:@typescript-eslint/recommended"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-types": ["error", { 
+        "types": { 
+          "{}": false
+        } 
+      }],
+    },
+  },
 ];
 
 export default eslintConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+//     ...compat.rules( {
+//       "@typescript-eslint/no-unsed-vars": "off",
+//       "@typescript-eslint/no-explicit-any": "off",  // `any`를 사용할 수 있도록 비활성화
+//       "@typescript-eslint/no-empty-object-type": "off",
+//     }),
+  
+// ];
+
+// export default eslintConfig;
 
